9147d2b3b9030a73639df26bf85ec0836d825386
vzlomshik-zhop <55409370+vzlomshik-zhop@users.noreply.github.com>
2020-01-04 15:43:36 +0200

    Update 4-do-while.js

--- Exercises/4-do-while.js
+++ Exercises/4-do-while.js
@@ -1,9 +1,17 @@
 'use strict';
 
+'use strict';
+
 const sum = (...args) => {
-  // Use do..while loop and accumulator variable
-  // to calculate sum of all given arguments
-  // For example sum(1, 2, 3) should return 6
+  let s = 0;
+  const n = args.length;
+  let i = 0;
+  do {
+    s += args[i++];
+  } while (i < n)
+  return s;
 };
+
+console.log(sum(1, 2, 3));
 
 module.exports = { sum };
